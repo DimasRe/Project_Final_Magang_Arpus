280e1b97ba1b015f9c988b4d7ff29b060ef7452e
Dimas Arta Ramadhani <131544573+DimasRe@users.noreply.github.com>
2025-06-10 10:24:10 +0700

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -6,7 +6,7 @@ from langchain_core.prompts import ChatPromptTemplate
 #path_file = "/content/drive/MyDrive/Celerates MSIB/ALL Dataset/Copy of API key Gemini.xlsx"
 #API_KEY = pd.read_excel(path_file)["api_key"][1]
 
-API_KEY = "AIzaSyAtdTs4aVol1viAQlPz1lthFUsCisGPQj0"
+API_KEY = ""
 
 def chat(contexts, history, question):
     llm = ChatGoogleGenerativeAI(
